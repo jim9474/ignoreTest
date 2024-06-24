77a672af44acad407009e8b24ec53f382600de1d
jim9474 <jim9474@naver.com>
2024-06-24 10:30:44 +0900

    plz ignore

---
+++ src/main/java/com/example/ignoreTest/IgnoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ignoreTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnoreTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ignoreTest/IgnoreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ignoreTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnoreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
